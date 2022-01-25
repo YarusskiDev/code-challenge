f5439dec0e9e20b743428e27a2cddfd1aaec54d9
Yarusski <crisvideoaulas@gmail.com>
2022-01-25 11:46:54 -0300

    refatorando2

--- domain/pessoaDomain.js
+++ domain/pessoaDomain.js
@@ -1,93 +1,98 @@
 const repoPessoa = require("../repository/pessoa");
 
 module.exports = {
-  validaPessoa(idPessoa) {
-    
-      const response = repoPessoa.buscarUm(idPessoa);
-      if(response){
-        return response;
-      }
-      else{
-        return {msgErro:"Erro do servidor"};
-      }
-    },
+  validaPessoa(id) {
 
-  ValidaCpf(cpf){
+    this.id = parseInt(id);
+
+    if (isNaN(this.id)) {
+      return { msgErro: "dado inconsistente" }
+    }
+    const response = repoPessoa.buscarUm(this.id);
+    if (response) {
+      return response;
+    }
+    else {
+      return { msgErro: "Erro do servidor" };
+    }
+  },
+
+  ValidaCpf(cpf) {
     return repoPessoa.buscarCpf(cpf);
   },
 
-  async validaPessoaExistente({nome,cpf,saldo}){
+  async validaPessoaExistente({ nome, cpf, saldo }) {
 
     this.nome = nome.toString();
     this.cpf = parseInt(cpf);
     this.saldo = parseInt(saldo);
 
-    const responseCpf =  await repoPessoa.buscarCpf(this.cpf);
-    
-    if(responseCpf){
-      return {msg:"esse CPF já tem uma conta!"} 
+    const responseCpf = await repoPessoa.buscarCpf(this.cpf);
+
+    if (responseCpf) {
+      return { msg: "esse CPF já tem uma conta!" }
     }
-    else{ 
-      return repoPessoa.criarPessoa(this.nome,this.cpf,this.saldo);
+    else {
+      return repoPessoa.criarPessoa(this.nome, this.cpf, this.saldo);
     }
   },
 
-  async validaEditar(id,nome){
+  async validaEditar(id, nome) {
 
     this.id = parseInt(id);
     this.nome = nome.toString();
-  
-    const response = await repoPessoa.editar(this.id,this.nome);
+
+    const response = await repoPessoa.editar(this.id, this.nome);
     return response;
   },
 
-  async validaTransferencia(idRemetente,Valor,idDestinatario){
+  async validaTransferencia(idRemetente, Valor, idDestinatario) {
     this.idRemetente = parseInt(idRemetente);
     this.Valor = parseFloat(Valor);
-    this.idDestinatario= parseInt(idDestinatario);
-    console.log(this.idRemetente,this.Valor,this.idDestinatario+"camada domain")
+    this.idDestinatario = parseInt(idDestinatario);
+    console.log(this.idRemetente, this.Valor, this.idDestinatario + "camada domain")
 
-    const response = await repoPessoa.transferir(this.idRemetente,this.Valor,this.idDestinatario);
+    const response = await repoPessoa.transferir(this.idRemetente, this.Valor, this.idDestinatario);
     console.log(response)
     return response;
   },
 
-  async validaDeposito(id,deposito){
+  async validaDeposito(id, deposito) {
 
     this.id = parseInt(id);
     this.deposito = parseFloat(deposito);
 
-    if(isNaN(this.id) || isNaN(this.deposito)){
-      return {msgErro:"valores inconsistentes"}
+    if (isNaN(this.id) || isNaN(this.deposito)) {
+      return { msgErro: "valores inconsistentes" }
     }
 
-    if (typeof(this.id) ==='number' && typeof(this.deposito ==='number')){
-      const response = await repoPessoa.deposito(this.id,this.deposito);
+    if (typeof (this.id) === 'number' && typeof (this.deposito === 'number')) {
+      const response = await repoPessoa.deposito(this.id, this.deposito);
       return response;
 
     }
-    else{
-      return {msgErro:"valores inconsistentes"}
+    else {
+      return { msgErro: "valores inconsistentes" }
     }
   },
-  
-  async validaDeletar(id){
+
+  async validaDeletar(id) {
     this.id = parseInt(id);
 
-    if(isNaN(this.id)){
-      return {msgErro:"valores inconsistente"}
+    if (isNaN(this.id)) {
+      return { msgErro: "valores inconsistente" }
     }
 
-    if(typeof(this.id)==='number'){
+    if (typeof (this.id) === 'number') {
       const response = await repoPessoa.deletar(this.id);
       return response;
     }
-    else{
-      return {msgErro:" id inconsistente"}
+    else {
+      return { msgErro: " id inconsistente" }
     }
   }
 
-  
+
 
 
 };
--- pessoas/PessoaController.js
+++ pessoas/PessoaController.js
@@ -1,26 +1,23 @@
 "use strict";
-const { Request, Response } = require("express");
+const { Request, Response, response } = require("express");
 const pessoaDomain = require("../domain/pessoaDomain");
 const modelPessoa = require("../pessoas/modelPessoa");
 
 module.exports = {
-  async buscaTodos(Request,Response) {
+  async buscaTodos(Request, Response) {
     const response = await modelPessoa.findAll();
     return Response.status(200).json(response);
   },
 
   async buscaUm(Request, Response) {
-    if (isNaN(Request.params.id)) {
-      Response.sendStatus(400);
-    } else {
-      let id = parseInt(Request.params.id);
-      const usuarioResposta = await pessoaDomain.validaPessoa(id);
-      Response.json(usuarioResposta);
-    }
+
+    const response = await pessoaDomain.validaPessoa(Request.params.id);
+    Response.json(response);
+
   },
 
   async criar(Request, Response) {
-    const body = {nome:Request.body.nome,cpf:Request.body.cpf,saldo:Request.body.saldo}
+    const body = { nome: Request.body.nome, cpf: Request.body.cpf, saldo: Request.body.saldo }
     const response = await pessoaDomain.validaPessoaExistente(body);
     return Response.status(201).json(response);
   },
@@ -28,10 +25,10 @@ module.exports = {
   async editar(Request, Response) {
     const id = Request.params.id;
     const body = Request.body.nome;
-    const response = await pessoaDomain.validaEditar(id,body);
+    const response = await pessoaDomain.validaEditar(id, body);
     return Response.status(200).json(response);
-    
-    
+
+
   },
 
   async transferir(Request, Response) {
@@ -40,13 +37,13 @@ module.exports = {
     const idDestinatario = Request.body.idDestinatario;
     const transferir = Request.body.transferir;
 
-    const response = await pessoaDomain.validaTransferencia(Request.params.id,Request.body.idDestinatario,Request.body.transferir);
+    const response = await pessoaDomain.validaTransferencia(Request.params.id, Request.body.idDestinatario, Request.body.transferir);
     return Response.json(response);
   },
 
   async deposito(Request, Response) {
 
-    const response = await pessoaDomain.validaDeposito(Request.params.id,Request.body.deposito);
+    const response = await pessoaDomain.validaDeposito(Request.params.id, Request.body.deposito);
     return Response.json(response);
   },
 
